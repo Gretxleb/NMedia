709d9b62009a5074a6dab4ee94e1fcb9aebb4be8
Sengel Paul <stasyaklose@gmail.com>
2026-02-12 11:36:16 +0100

    Add PostDetailsFragment and navigation logic

--- app/src/main/java/ru/netology/nmedia/activity/FeedFragment.kt
+++ app/src/main/java/ru/netology/nmedia/activity/FeedFragment.kt
@@ -1,5 +1,6 @@
 package ru.netology.nmedia.activity
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -13,24 +14,55 @@ import ru.netology.nmedia.adapter.PostAdapter
 import ru.netology.nmedia.databinding.FragmentFeedBinding
 import ru.netology.nmedia.dto.Post
 import ru.netology.nmedia.viewmodel.PostViewModel
+import ru.netology.nmedia.activity.PostDetailsFragment.Companion.idArg
 
 class FeedFragment : Fragment() {
+
+    private val viewModel: PostViewModel by viewModels(ownerProducer = ::requireParentFragment)
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         val binding = FragmentFeedBinding.inflate(inflater, container, false)
-        val viewModel: PostViewModel by viewModels(ownerProducer = ::requireParentFragment)
 
         val adapter = PostAdapter(object : OnInteractionListener {
-            override fun onLike(post: Post) = viewModel.likeById(post.id)
-            override fun onShare(post: Post) = viewModel.shareById(post.id)
+            override fun onEdit(post: Post) {
+                viewModel.edit(post)
+            }
+
+            override fun onLike(post: Post) {
+                viewModel.likeById(post.id)
+            }
+
+            override fun onRemove(post: Post) {
+                viewModel.removeById(post.id)
+            }
+
+            override fun onShare(post: Post) {
+                val intent = Intent().apply {
+                    action = Intent.ACTION_SEND
+                    putExtra(Intent.EXTRA_TEXT, post.content)
+                    type = "text/plain"
+                }
+                val shareIntent = Intent.createChooser(intent, getString(R.string.chooser_share_post))
+                startActivity(shareIntent)
+            }
+
+            override fun onPost(post: Post) {
+                findNavController().navigate(
+                    R.id.action_feedFragment_to_postDetailsFragment,
+                    Bundle().apply {
+                        idArg = post.id
+                    }
+                )
+            }
         })
 
         binding.list.adapter = adapter
-        viewModel.data.observe(viewLifecycleOwner) { posts ->
-            adapter.submitList(posts)
+        viewModel.data.observe(viewLifecycleOwner) { state ->
+            adapter.submitList(state.posts)
         }
 
         binding.fab.setOnClickListener {
---
+++ app/src/main/java/ru/netology/nmedia/activity/PostDetailsFragment.kt
@@ -0,0 +1,70 @@
+package ru.netology.nmedia.activity
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import ru.netology.nmedia.R
+import ru.netology.nmedia.adapter.PostViewHolder
+import ru.netology.nmedia.databinding.FragmentPostDetailsBinding
+import ru.netology.nmedia.util.LongArg
+import ru.netology.nmedia.viewmodel.PostViewModel
+import ru.netology.nmedia.dto.Post
+
+class PostDetailsFragment : Fragment() {
+
+    companion object {
+        var Bundle.idArg: Long by LongArg
+    }
+
+    private val viewModel: PostViewModel by viewModels(ownerProducer = ::requireParentFragment)
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val binding = FragmentPostDetailsBinding.inflate(inflater, container, false)
+
+        val postId = arguments?.idArg ?: -1L
+
+        viewModel.data.observe(viewLifecycleOwner) { state ->
+            val post = state.posts.find { it.id == postId } ?: run {
+                findNavController().navigateUp()
+                return@observe
+            }
+
+            val viewHolder = PostViewHolder(binding.post_layout, object : ru.netology.nmedia.adapter.OnInteractionListener {
+                override fun onLike(post: Post) {
+                    viewModel.likeById(post.id)
+                }
+
+                override fun onShare(post: Post) {
+                    viewModel.shareById(post.id)
+                }
+
+                override fun onRemove(post: Post) {
+                    viewModel.removeById(post.id)
+                    findNavController().navigateUp()
+                }
+
+                override fun onEdit(post: Post) {
+                    viewModel.edit(post)
+                    findNavController().navigate(
+                        R.id.action_postDetailsFragment_to_newPostFragment,
+                        Bundle().apply {
+                            textArg = post.content
+                        }
+                    )
+                }
+            })
+
+            viewHolder.bind(post)
+        }
+
+        return binding.root
+    }
+}
