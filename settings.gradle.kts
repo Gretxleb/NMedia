f4e0f48ec929d5ba89bee9f25f7deebb35dda4ed
Sengel Paul <stasyaklose@gmail.com>
2026-02-12 12:42:43 +0100

    Добавлены конфигурационные файлы

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,7 +11,23 @@ dependencyResolutionManagement {
         google()
         mavenCentral()
     }
+}pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+    }
 }
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+rootProject.name = "NMedia"
+include(":app")
+
 
 rootProject.name = "NMedia"
 include(":app")
