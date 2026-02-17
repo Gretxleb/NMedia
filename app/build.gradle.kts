ab575d8a0c2bb61419c649302be91eb763e6e588
Sengel Paul <stasyaklose@gmail.com>
2026-02-17 13:20:18 +0100

    fix gradle version catalog references

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,8 +41,8 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.navigation.fragment.ktx)
-    implementation(libs.androidx.navigation.ui-ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso-core)
+    androidTestImplementation(libs.androidx.espresso.core)
 }
