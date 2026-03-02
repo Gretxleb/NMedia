c46ae3b4b3096c21e159f0704e9f21d215a4dfaa
Sengel Paul <stasyaklose@gmail.com>
2026-03-02 12:01:38 +0100

    Fix CI: add pluginManagement with google() and define plugin versions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,8 +2,6 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("kotlin-kapt")
-    id("androidx.navigation.safeargs.kotlin")
-    id("com.google.gms.google-services")
 }
 
 android {
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,4 @@
-buildscript {
-    dependencies {
-        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.7.7")
-        classpath("com.google.gms:google-services:4.4.1")
-    }
-}
-
 plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
-    id("kotlin-kapt")
+    id("com.android.application") version "8.2.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
 }
