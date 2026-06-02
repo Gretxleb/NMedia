e7edfadd969cf877fe8318aa2ca0d5a93408079d
Sengel Paul <stasyaklose@gmail.com>
2026-06-02 20:48:03 +0200

    fix: add kotlinx-metadata-jvm dependency to resolve Hilt metadata version error

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,6 +59,8 @@ dependencies {
     annotationProcessor("androidx.room:room-compiler:2.7.1")
     kapt("androidx.room:room-compiler:2.7.1")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-metadata-jvm:0.9.0")
     implementation("com.google.dagger:hilt-android:2.51.1")
     kapt("com.google.dagger:hilt-compiler:2.51.1")
 }
+
